c887ad74198e1540173e4584a66aff049de1a56d
jd <barasonadave@gmail.com>
2022-10-06 20:16:06 +0800

    updated application.properties

---
+++ src/main/java/com/innovation/control/MasterControlApplication.java
@@ -0,0 +1,13 @@
+package com.innovation.control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterControlApplication.class, args);
+	}
+
+}
